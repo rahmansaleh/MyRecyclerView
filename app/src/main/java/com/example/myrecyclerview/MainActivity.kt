f178e8df72ab81fdd4de729f90868c22e7b773b8
muhammad rahman <rahman9600@gmail.com>
2019-08-10 17:05:57 +0700

    Update MainActivity.kt
    - init data recyclerview
    - init ui recyclerview
    - init databinding recyclerview

--- app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.myrecyclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.example.myrecyclerview.model.Hero
+import com.example.myrecyclerview.model.HeroesData
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_hero)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
