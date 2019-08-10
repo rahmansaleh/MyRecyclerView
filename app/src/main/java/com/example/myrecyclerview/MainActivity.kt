ab50a75d9d973fd6ba0ccb49e908163f21920582
muhammad rahman <rahman9600@gmail.com>
2019-08-10 17:19:29 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import com.example.myrecyclerview.model.Hero
 import com.example.myrecyclerview.model.HeroesData
 
@@ -22,10 +24,39 @@ class MainActivity : AppCompatActivity() {
         showRecyclerList()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        setMode(item!!.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun showRecyclerList() {
 
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+    }
+
+
+
+    private fun setMode(selectedMenu: Int) {
+        when(selectedMenu) {
+            R.id.action_list -> {
+
+            }
+
+            R.id.action_grid -> {
+
+            }
+
+            R.id.action_cardview -> {
+
+            }
+        }
     }
 }
