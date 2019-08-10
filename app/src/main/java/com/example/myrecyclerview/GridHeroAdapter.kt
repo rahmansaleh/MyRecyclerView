fdf857d00e59cac1b7b52cc5a8636c8cf2661011
muhammad rahman <rahman9600@gmail.com>
2019-08-10 17:24:20 +0700

    Create GridHeroAdapter.kt

---
+++ app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.myrecyclerview
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class GridHeroAdapter : RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): GridHeroAdapter.GridViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: GridHeroAdapter.GridViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+}
