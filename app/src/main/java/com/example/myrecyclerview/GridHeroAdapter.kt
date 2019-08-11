bf5d2cafb82d3b6689fa9f35016250ef640fa8ca
muhammad rahman <rahman9600@gmail.com>
2019-08-11 10:59:36 +0700

    Update GridHeroAdapter.kt
    adding interface

--- app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.kt
+++ app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.kt
@@ -10,6 +10,12 @@ import com.example.myrecyclerview.model.Hero
 
 class GridHeroAdapter(val listHero: ArrayList<Hero>) : RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
 
+    private lateinit var onItemClickCallBack: OnItemClickCallBack
+
+    fun setOnItemClickCallBack(onItemClickCallBack: OnItemClickCallBack) {
+        this.onItemClickCallBack = onItemClickCallBack
+    }
+
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): GridHeroAdapter.GridViewHolder {
         val view: View = LayoutInflater.from(p0.context).inflate(R.layout.item_grid_hero, p0, false)
         return GridViewHolder(view)
@@ -23,10 +29,16 @@ class GridHeroAdapter(val listHero: ArrayList<Hero>) : RecyclerView.Adapter<Grid
         Glide.with(p0.itemView.context)
             .load(listHero[p1].photo)
             .into(p0.imgPhoto)
+
+        p0.itemView.setOnClickListener { onItemClickCallBack.onItemCLicked( listHero[p1] ) }
     }
 
     inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    interface OnItemClickCallBack{
+        fun onItemCLicked(data: Hero)
     }
 
 }
